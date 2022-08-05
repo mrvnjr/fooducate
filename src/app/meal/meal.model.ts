8a49a97d104bd36b26316e0436694ab719321bed
mrvnjr <marvin.magboojr@g.batstate-u.edu.ph>
2022-08-05 15:44:45 +0800

    meal&healthymeal done

--- src/app/meal/meal.model.ts
+++ src/app/meal/meal.model.ts
@@ -1,7 +1,18 @@
 export interface Meal{
     plans: string;
     name: string;
-    breakfast: string;
+    bf1: string;
+    bname: string;
+    bf2: string;
+    b2name: string;
+    bt: string;
+    btname: string;
     lunch: string;
-    dinner: string;
+    lname: string;
+    lt: string;
+    ltname:string;
+    d1: string;
+    d2: string;
+    d1name: string;
+    d2name: string;
     }
